1eb35c5d3deb172470a18fccbacbb5ef9d08c18a
Jerry Thomas <me@jerrythomas.name>
2022-06-04 05:55:50 +0530

    Added container check

--- spec/ProgressDots.spec.js
+++ spec/ProgressDots.spec.js
@@ -3,34 +3,35 @@ import { cleanup, render, fireEvent } from '@testing-library/svelte'
 import ProgressDots from '../src/ProgressDots.svelte'
 
 describe('ProgressDots.svelte', () => {
-  beforeEach(() => cleanup())
-  it('should allow clicking first item only', async () => {
-    const { container } = render(ProgressDots, {
-      count: 5,
-      value: -1,
-      current: -1,
-    })
-    let dots = container.querySelectorAll('.step')
-    validateDots(dots, 5, -1, -1)
+	beforeEach(() => cleanup())
+	it('should allow clicking first item only', async () => {
+		const { container } = render(ProgressDots, {
+			count: 5,
+			value: -1,
+			current: -1
+		})
+		expect(container).toBeTruthy()
+		let dots = container.querySelectorAll('.step')
+		validateDots(dots, 5, -1, -1)
 
-    // await fireEvent.click(dots[0])
-    // validateDots(dots, 5, -1, 0)
-  })
+		// await fireEvent.click(dots[0])
+		// validateDots(dots, 5, -1, 0)
+	})
 })
 
 function validateDots(dots, count, value, current) {
-  expect(dots.length).toEqual(count)
+	expect(dots.length).toEqual(count)
 
-  dots.forEach((d, i) => {
-    expect(d.classList.contains('step')).toBeTruthy()
-    expect(d.classList.contains('flex')).toBeTruthy()
-    expect(d.classList.contains('w-2')).toBeTruthy()
-    expect(d.classList.contains('h-2')).toBeTruthy()
-    expect(d.classList.contains('mx-1')).toBeTruthy()
-    expect(d.classList.contains('rounded-full')).toBeTruthy()
-    expect(d.dataset.step).toEqual(i.toString())
-    expect(d.dataset.active).toEqual((i == current).toString())
-    expect(d.dataset.completed).toEqual((i <= value).toString())
-    expect(d.dataset.clickable).toEqual((i <= value + 1).toString())
-  })
+	dots.forEach((d, i) => {
+		expect(d.classList.contains('step')).toBeTruthy()
+		expect(d.classList.contains('flex')).toBeTruthy()
+		expect(d.classList.contains('w-2')).toBeTruthy()
+		expect(d.classList.contains('h-2')).toBeTruthy()
+		expect(d.classList.contains('mx-1')).toBeTruthy()
+		expect(d.classList.contains('rounded-full')).toBeTruthy()
+		expect(d.dataset.step).toEqual(i.toString())
+		expect(d.dataset.active).toEqual((i == current).toString())
+		expect(d.dataset.completed).toEqual((i <= value).toString())
+		expect(d.dataset.clickable).toEqual((i <= value + 1).toString())
+	})
 }
